a488573cc0be8675762c7ff09d6f029cc0a9d452
Tate Redding <tateredding@gmail.com>
2023-05-23 09:58:14 -0600

    deleting confirmation

--- src/components/Features/SmallFeatureCard.js
+++ src/components/Features/SmallFeatureCard.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const SmallFeatureCard = ({ feature, getFeatures }) => {
+    const [deleting, setDeleting] = useState(false);
+
     const navigate = useNavigate();
 
     const deleteFeature = async () => {
         try {
             const response = await axios.delete(`/api/features/${feature.id}`);
-            console.log(response.data);
             if (response.data) {
+                setDeleting(false);
                 getFeatures();
             };
         } catch (error) {
@@ -17,12 +19,27 @@ const SmallFeatureCard = ({ feature, getFeatures }) => {
         };
     };
 
-    return(
-        <div className="card">
-            <h5 className="card-title">{feature.name}</h5>
-            <button className="btn btn-primary" onClick={() => navigate(`/features/edit/${feature.id}`)}>Edit</button>
-            <button className="btn btn-danger" onClick={deleteFeature}>Delete</button>
-        </div>
+    return (
+        <>
+            {
+                (deleting) ?
+                    <div className="card mb-3">
+                        <div className="card-body">
+                            <h5 className="card-title">Are you sure you want to delete {feature.name}?</h5>
+                            <button className="btn btn-primary me-2" onClick={() => setDeleting(false)}>No</button>
+                            <button className="btn btn-danger" onClick={deleteFeature}>Yes</button>
+                        </div>
+                    </div> :
+                    <div className="card mb-3">
+                        <div className="card-body">
+                            <h5 className="card-title">{feature.name}</h5>
+                            <button className="btn btn-primary me-2" onClick={() => navigate(`/features/edit/${feature.id}`)}>Edit</button>
+                            <button className="btn btn-danger" onClick={() => setDeleting(true)}>Delete</button>
+                        </div>
+                    </div>
+            }
+        </>
+
     );
 };
 
--- src/components/Spells/SmallSpellCard.js
+++ src/components/Spells/SmallSpellCard.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const SmallSpellCard = ({ spell, getSpells }) => {
+    const [deleting, setDeleting] = useState(false);
+
     const navigate = useNavigate();
 
     const deleteSpell = async () => {
         try {
             const response = await axios.delete(`/api/spells/${spell.id}`);
             if (response.data) {
+                setDeleting(false);
                 getSpells();
             };
         } catch (error) {
@@ -16,12 +19,26 @@ const SmallSpellCard = ({ spell, getSpells }) => {
         };
     };
 
-    return(
-        <div className="card">
-            <h5 className="card-title">{spell.name}</h5>
-            <button className="btn btn-primary" onClick={() => navigate(`/spells/edit/${spell.id}`)}>Edit</button>
-            <button className="btn btn-danger" onClick={deleteSpell}>Delete</button>
-        </div>
+    return (
+        <>
+            {
+                (deleting) ?
+                    <div className="card mb-3">
+                        <div className="card-body">
+                            <h5 className="card-title">Are you sure you want to delete {spell.name}?</h5>
+                            <button className="btn btn-primary me-2" onClick={() => setDeleting(false)}>No</button>
+                            <button className="btn btn-danger" onClick={deleteSpell}>Yes</button>
+                        </div>
+                    </div> :
+                    <div className="card mb-3">
+                        <div className="card-body">
+                            <h5 className="card-title">{spell.name}</h5>
+                            <button className="btn btn-primary me-2" onClick={() => navigate(`/spells/edit/${spell.id}`)}>Edit</button>
+                            <button className="btn btn-danger" onClick={() => setDeleting(true)}>Delete</button>
+                        </div>
+                    </div>
+            }
+        </>
     );
 };
 
