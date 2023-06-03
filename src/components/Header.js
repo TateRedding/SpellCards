7621a078a52024d921d96615b2f4e580c6b1a007
Tate Redding <tateredding@gmail.com>
2023-06-03 12:46:01 -0600

    header update and consolidation

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,17 +12,24 @@ const Header = ({ players }) => {
                     </button>
                     <div className="collapse navbar-collapse" id="playerButtons">
                         <ul className="navbar-nav me-auto mb-2 mb-sm-0">
-                            {
-                                players.map((player) => {
-                                    return (
-                                        <li className="nav-item" key={player.id}>
-                                            <Link className="nav-link" to={`/${player.name.toLowerCase()}`}>
-                                                <span data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">{player.name}</span>
-                                            </Link>
-                                        </li>
-                                    );
-                                })
-                            }
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                                    Players
+                                </a>
+                                <ul className="dropdown-menu">
+                                    {
+                                        players.map((player) => {
+                                            return (
+                                                <li className="nav-item" key={player.id}>
+                                                    <Link className="nav-link" to={`/${player.name.toLowerCase()}`}>
+                                                        <span data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">{player.name}</span>
+                                                    </Link>
+                                                </li>
+                                            );
+                                        })
+                                    }
+                                </ul>
+                            </li>
                             <li className="nav-item">
                                 <Link className="nav-link" to="/spells">
                                     <span data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Spells</span>
@@ -31,6 +38,16 @@ const Header = ({ players }) => {
                             <li className="nav-item">
                                 <Link className="nav-link" to="/features">
                                     <span data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Features</span>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/items">
+                                    <span data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Items</span>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/quests">
+                                    <span data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Quests</span>
                                 </Link>
                             </li>
                         </ul>
